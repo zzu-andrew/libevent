12d1624d4c53e45ef4c09612e5e1ca9926153dd7
zzu-andrew <564631192@qq.com>
2020-07-05 19:22:51 +0800

    添加完整的事件驱动libevent库和测试程序

---
+++ inc/event_interface.h
@@ -0,0 +1,23 @@
+/**
+ *  事件驱动interface接口
+ * 
+ * */
+
+#ifndef __EVENT_INTERFACE__
+#define __EVENT_INTERFACE__
+
+/* MACRO DEFINE */
+#define ERROR        -1
+#define OK            0
+
+#define SUCCESS       0
+#define FAILED        1
+
+
+
+
+
+
+
+
+#endif // !__EVENT_INTERFACE__
---
+++ test/first_libevent/first_libevent.cpp
@@ -0,0 +1,19 @@
+﻿/***
+ *  创建event base
+ * */
+
+#include <event2/event.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    std::cout << "test libevent!\n"; 
+	//创建libevent的上下文
+	event_base * base = event_base_new();
+	if (base)
+	{
+		cout << "event_base_new success!" << endl;
+	}
+	return 0;
+}
