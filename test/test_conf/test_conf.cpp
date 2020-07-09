9e71c668097f0be0666acd4bcf3fd1ef4edd039f
zzu-andrew <564631192@qq.com>
2020-07-09 21:32:45 +0800

    添加制定method的方法

--- test/test_conf/test_conf.cpp
+++ test/test_conf/test_conf.cpp
@@ -15,6 +15,7 @@ int main()
 		return 1;
 
 	//创建配置上下文
+	//该对象创建出来是为了配置事件的规则信息使用的
 	event_config *config = event_config_new();
 	//显示支持的网络模式
 	const char **methods = 	event_get_supported_methods();
@@ -27,17 +28,24 @@ int main()
 	//这个features在linux中设置没有效果，因为linux中本来就是支持ET模式的，边缘触发模式
 	// 设置了EV_FEATURE_FDS其他特征嗯就无法设置
 	//也就是所支持了EV_FEATURE_FDS  其他的特征都是无法支持的
-	int ret = 
-	event_config_require_features(config, EV_FEATURE_ET|EV_FEATURE_EARLY_CLOSE);
+	// event_config_require_features(config, EV_FEATURE_FDS); //不支持epoll
+	// event_config_require_features(config, EV_FEATURE_FDS); //设置网络模型使用select
+	event_config_avoid_method(config, "epoll");
+	event_config_avoid_method(config, "poll");
+	/*
+	int ret = event_config_require_features(config, EV_FEATURE_ET|EV_FEATURE_EARLY_CLOSE);
 	if(OK != ret)
 	{
 		cerr << "event config require features failed." << endl;
 		return ERROR; 
 	}
+	*/
 	//初始化libevent上下文
+	//这个上下的茶创建是随着配置信息生成的
 	event_base *base =  event_base_new_with_config(config);
 
 	//config一旦配置好就不需要在使用了
+	//也就是所所有的配置信息需要在这之前进行销毁
 	event_config_free(config);
 
 	if(!base)
@@ -53,6 +61,12 @@ int main()
 	}
 	else
 	{
+		//获取当前网路模型
+		const char * pBaseName = NULL;
+		pBaseName = event_base_get_method(base);
+		cout << "suport method:" << pBaseName << endl;
+
+
 		//确认特征那些生效
 		int f = event_base_get_features(base);
 		if(f&EV_FEATURE_ET)
